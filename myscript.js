8c6abe681675e84214a7e39bfb72a4ba1aa0fc0f
bschroen <brooke.schroen21@gmail.com>
2016-09-07 20:57:19 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,77 @@
+function change01(){
+	document.getElementById("large").src="images/moth01.jpg";
+}
+function change01(){
+	document.getElementById("caption").innerHTML = "moth1" ;
+}
+function change02(){
+	document.getElementById("large").src="images/moth02.jpg";
+}
+
+function change02(){
+	document.getElementById("caption").innerHTML = "moth2" ;
+}
+
+function change03(){
+	document.getElementById("large").src="images/moth03.jpg";
+}
+
+function change03(){
+	document.getElementById("caption").innerHTML = "moth3" ;
+}
+
+function change04(){
+	document.getElementById("large").src="images/moth04.jpg";
+}
+
+function change04(){
+	document.getElementById("caption").innerHTML = "moth4" ;
+}
+
+function change05(){
+	document.getElementById("large").src="images/moth5.jpg";
+}
+
+function change05(){
+	document.getElementById("caption").innerHTML = "moth5" ;
+}
+
+function change06(){
+	document.getElementById("large").src="images/moth6.jpg";
+}
+
+function change06(){
+	document.getElementById("caption").innerHTML = "moth6" ;
+}
+
+function change07(){
+	document.getElementById("large").src="images/moth07.jpg";
+}
+
+function change07(){
+	document.getElementById("caption").innerHTML = "moth7" ;
+}
+
+function change08(){
+	document.getElementById("large").src="images/moth08.jpg";
+}
+
+function change08(){
+	document.getElementById("caption").innerHTML = "moth8" ;
+}
+
+function change09(){
+	document.getElementById("large").src="images/moth09.jpg";
+}
+
+function change09(){
+	document.getElementById("caption").innerHTML = "moth9" ;
+}
+
+function change10(){
+	document.getElementById("large").src="images/moth10.jpg";
+}
+
+function change10(){
+	document.getElementById("caption").innerHTML = "moth10" ;
+}
